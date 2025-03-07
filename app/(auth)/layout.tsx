9eb423cb4b88a11ff884b2e3069e1c2a42365cbe
Arpit Rautela <arpitrautela40@gmail.com>
2025-03-08 01:45:33 +0530

    initial commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+export default function RootLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+      <html lang="en">
+        <body>
+          <main>{children}</main>
+        </body>
+      </html>
+    );
+  }
+  

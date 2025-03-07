fc834b637f272e1ced0d67096c3f724618671879
Arpit Rautela <arpitrautela40@gmail.com>
2025-03-08 02:56:21 +0530

    adding pages

---
+++ Components/HeaderBox.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HeaderBox = ({type = "title",title,user,subtext}:HeaderBoxProps) => {
+  return (
+    <div className='header-box'>
+        <h1 className="header-box-title ">
+            {title}
+            {type === 'greeting' && (
+                <span> {user}</span>
+            )}
+        </h1>
+    </div>
+  )
+}
+
+export default HeaderBox
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,22 @@
+import HeaderBox from '@/Components/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName: 'Arpit'};
+
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className='home-content'>
+        <div className='home-header'>
+          <HeaderBox
+          type="greeting"
+          title="Welcome"
+          user = {loggedIn?.firstName || 'Guest'}
+          subtext = "Access and manage your account and transaction efficiently" 
+          />
+        </div>
+      </div>
+    </section>
   )
 }
 
